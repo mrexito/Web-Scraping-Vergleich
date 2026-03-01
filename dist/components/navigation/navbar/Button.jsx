23d11deea761b83f4ba415a9d1c9cd7998204940
TamaStefBfh <https://github.com/TamaStefBfh/gymi_course_crawler.git>
2026-03-01 22:54:26 +0100

    Remove build artifacts from repository

--- dist/components/navigation/navbar/Button.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Button = function () {
-    return (<button className="h-12 rounded-lg bg-cr-darkgrey text-white font-bold px-5">
-      Sign In
-    </button>);
-};
-exports.default = Button;
